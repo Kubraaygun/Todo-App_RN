94ea095c094109e50bf766f2bf33788bb9d683d4
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2025-05-29 18:17:25 +0300

    add styles

---
+++ .history/app/index_20250529145737.tsx
@@ -0,0 +1,48 @@
+import { FlatList, View } from "react-native";
+
+const todoData = [
+  {
+    id: 1,
+    title: "Todo 1",
+    isDone: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    isDone: false,
+  },
+  {
+    id: 3,
+    title: "Todo 3",
+    isDone: false,
+  },
+  {
+    id: 4,
+    title: "Todo 4",
+    isDone: true,
+  },
+  {
+    id: 5,
+    title: "Todo 5",
+    isDone: false,
+  },
+  {
+    id: 6,
+    title: "Todo 6",
+    isDone: false,
+  },
+];
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <FlatList data={todoData} keyExtractor={(item) => item.id.toString()} />
+    </View>
+  );
+}
---
+++ .history/app/index_20250529145939.tsx
@@ -0,0 +1,51 @@
+import { FlatList, View } from "react-native";
+
+const todoData = [
+  {
+    id: 1,
+    title: "Todo 1",
+    isDone: false,
+  },
+  {
+    id: 2,
+    title: "Todo 2",
+    isDone: false,
+  },
+  {
+    id: 3,
+    title: "Todo 3",
+    isDone: false,
+  },
+  {
+    id: 4,
+    title: "Todo 4",
+    isDone: true,
+  },
+  {
+    id: 5,
+    title: "Todo 5",
+    isDone: false,
+  },
+  {
+    id: 6,
+    title: "Todo 6",
+    isDone: false,
+  },
+];
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {/* data=Listelenecek verileri iceren dizi */}
+      {/* keyExtractor=Her item için benzersiz bir key belirler.  */}
+            {/* data=Listelenecek verileri iceren dizi */}
+      <FlatList data={todoData} keyExtractor={(item) => item.id.toString() renderItem} />
+    </View>
+  );
+}
